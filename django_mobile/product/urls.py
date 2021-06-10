8c7dc0a1b1d72e2d90a5d68853e5e9d1521cd41e
anjana-analyst <anjanasrinivasamoorthy@gmail.com>
2021-06-10 23:09:57 +0530

    First Commit

---
+++ django_mobile/product/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path,include
+from .views import LatestProductsList, ProductDetail,CategoryDetail
+
+urlpatterns = [
+    path('latest-products/',LatestProductsList.as_view()),
+    path('products/<slug:category_slug>/<slug:product_slug>/',ProductDetail.as_view()),
+    path('products/<slug:category_slug>/',CategoryDetail.as_view())
+]
+
